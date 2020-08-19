b883aec3979639104b8fada3055dc86f451fc940
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-08-19 09:20:08 -0700

    fix mistakes

--- function6.js
+++ function6.js
@@ -4,7 +4,14 @@ let food = ['apple', 'orange', 'cake', 'carrots', 'pasta', 'rice', 'eggs'];
 let numbers = ['1', '2', '3', '4'];
 
 const addTheLast = function (arr1, arr2) {
-    console.log(arr2[arr2.length-1] += arr1[arr1.length-1]);
+
+    console.log(arr1);
+    console.log(arr2);
+
+    console.log("*****adds it to the end of the first array******")
+    arr1.push(arr2[arr2.length-1])
+    console.log(arr1);
+    // console.log(arr2[arr2.length-1] += arr1[arr1.length-1]);
 };
 
 addTheLast(food, numbers);
